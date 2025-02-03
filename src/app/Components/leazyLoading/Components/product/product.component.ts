3a19a3933e6444ce3ef440f86c0d85030f58204e
gaikwadGFS <kshitija.gaikwad@growingflowers-solutions.com>
2025-02-03 18:47:29 +0530

    CRUD Operation

--- src/app/Components/leazyLoading/Components/product/product.component.ts
+++ src/app/Components/leazyLoading/Components/product/product.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UsersService } from '../../../../Services/user/users.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   providers:[UsersService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
@@ -13,6 +14,12 @@ import { CommonModule } from '@angular/common';
 export class ProductComponent {
 
   data:any[]=[];
+  postsObj:any={
+    userId: 0,
+    id: 0,
+    title: "",
+    body: ""
+  }
 
   constructor(private userSrv:UsersService){
     this.getAllPost();
@@ -22,6 +29,40 @@ export class ProductComponent {
   getAllPost(){
     this.userSrv.getAllPostData().subscribe((response:any)=>{
     this.data=response;
+    })
+  }
+
+  onSave(){
+     this.userSrv.createPosts(this.postsObj).subscribe((response:any)=>{
+      console.log(response);
+      alert("Form Successfully Sumbitted")
+     })
+  }
+  onClear(){
+    this.postsObj={
+      userId: 0,
+      id: 0,
+      title: "",
+      body: ""
+    }
+  }
+
+  onEdit(record:any){
+    this.postsObj=record;
+  }
+
+  onUpdate(){
+    this.userSrv.updatePosts(this.postsObj).subscribe((result:any)=>{
+      console.log(result);
+      alert("Updated successfull");
+      this.onClear();
+    })
+  }
+
+  onDelete(id:number){
+    this.userSrv.deleteRecord(id).subscribe((record:any)=>{
+      console.log(record);
+      alert("Record deleted Successfull")
     })
   }
 }
--- src/app/Services/user/users.service.ts
+++ src/app/Services/user/users.service.ts
@@ -13,7 +13,18 @@ export class UsersService {
   return this.http.get("https://jsonplaceholder.typicode.com/posts");
   }
 
-  allData(){
+  createPosts(object:any):Observable<any>{
+    return this.http.post("https://jsonplaceholder.typicode.com/posts",object);
+  }
 
+  updatePosts(obj:any):Observable<any>{
+   return this.http.post("https://jsonplaceholder.typicode.com/posts",obj);
+  }
+
+  deleteRecord(id:number):Observable<any>{
+    // return this.http.delete("https://jsonplaceholder.typicode.com/posts/"+id);
+    return this.http.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+      // https://jsonplaceholder.typicode.com/posts/1
   }
 }
